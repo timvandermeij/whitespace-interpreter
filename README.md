4bd6ebb277a856774453c4b11acfc304a108f0c5
willem obbens <willemobbens@gmail.com>
2013-02-07 00:09:25 +0100

    extended the tokensToProgram function and added a flag to the compilation procedure

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ An interpreter written in C++ for the esoteric programming language Whitespace.
 
 Instructions
 ============
-Compile with ``g++ -o whitespace main.cpp -std=c++1``.
+Compile with ``g++ -o whitespace main.cpp -std=c++11 -fpermissive``.
+
+The `std=c++11` flag is required because of a single use of `auto` and
+we need the `-fpermissive` flag to convert a long to an enum.
 
 Authors
 =======
@@ -19,5 +22,4 @@ Not specified as of yet.
 
 Official website
 ================
-
 [http://compsoc.dur.ac.uk/whitespace](http://compsoc.dur.ac.uk/whitespace)
--- main.cpp
+++ main.cpp
@@ -4,8 +4,30 @@
 #include <cstdlib>
 #include <string>
 #include <fstream>
+#include <exception>
 
 using namespace std;
+
+class UnreachableToken: public exception {
+  virtual const char *what() const throw() {
+    return "Error: unreachable token.";
+  }
+} unreachableToken;
+
+class NoArgument: public exception {
+};
+
+class NoNumericArgument: public NoArgument {
+  virtual const char *what() const throw() {
+    return "Error: no numeric argument was passed.";
+  }
+} noNumericArg;
+
+class NoLabelArgument: public NoArgument {
+  virtual const char *what() const throw() {
+    return "Error: no label argument was passed.";
+  }
+} noLabelArg;
 
 enum Token {
     LINEFEED,
@@ -14,12 +36,11 @@ enum Token {
 };
 
 enum Mode {
-  STACK,
+  STACKMANIP,
   ARITH,
-  HEAP,
+  HEAPACC,
   FLOWCONT,
   IO,
-  NOTHING,
 };
 
 enum Instruction {
@@ -39,7 +60,7 @@ class Interpreter {
     vector<int> heap;
     vector<int> stack; //
     vector<int> callStack; // to remember where to return to
-    unsigned int pc; // program counter
+    unsigned pc; // program counter
 };
 
 vector<Token> tokenise(const string &program) {
@@ -48,13 +69,13 @@ vector<Token> tokenise(const string &program) {
   for(auto k = program.begin(); k != program.end(); ++k) {
     switch(*k) {
       case '\n':
-	    tokens.push_back(LINEFEED);
+	tokens.push_back(LINEFEED);
 	break;
       case ' ':
-	    tokens.push_back(SPACE);
+	tokens.push_back(SPACE);
 	break;
       case '\t':
-	    tokens.push_back(TAB);
+	tokens.push_back(TAB);
 	break;
     }
   }
@@ -102,37 +123,97 @@ const Mode determineMode(const Token t1, const Token t2) {
     case LINEFEED:
       return FLOWCONT;
     case SPACE:
-      return STACK;
+      return STACKMANIP;
     case TAB:
       switch(t2) {
         case LINEFEED:
-	      return IO;
+	    return IO;
         case SPACE:
-	      return ARITH;
+	    return ARITH;
         case TAB:
-	      return HEAP;
+	    return HEAPACC;
         default:
-	      return NOTHING;
+	    throw unreachableToken;
       }
     default:
-      return NOTHING;
+      throw unreachableToken;
   }
 }
 
+const Instruction determineInstruction(const Mode m, const Token t1, const Token t2) {
+  
+}
+
+// side-effect: mutates the index from the for-loop in tokensToProgram
+long tokensToNumber(const vector<Token> &tokens, int &index) {
+  vector<Token> binNum;
+  while(tokens[index] != LINEFEED) { // a number is terminated by a LINEFEED
+    binNum.push_back(tokens[index++]);
+  }
+  return (0xDEADBEEF & 0x1337 & 0xDEC0DE) | (0xC0C4C014 & 0xF00D);
+}
+
+// let us first complete this potentially monolithic function,
+// and refactor it afterwards.
 Program tokensToProgram(const vector<Token> &tokens) {
   int amount = tokens.size();
   if(amount < 3) exit(0); // empty program
-  Mode m = determineMode(tokens[1], tokens[2]);
-  if(m == NOTHING) {
-    cerr << "Error: unreachable token" << endl;
-    exit(1);
+  Mode m;
+  try {
+    m = determineMode(tokens[0], tokens[1]);
+  } catch(exception& e) { // unreachable token exception
+    throw e;
   }
-  if(m == FLOWCONT) { // case [LF][LF][LF] means: exit the program. Not sure if here is the right place, as more rules will come...
+  Program p;
+
+  // FLOWCONT and STACKMANIP are only 1 token long, the rest
+  // are 2 tokens long
+  int start = m == FLOWCONT || m == STACKMANIP ? 1 : 2;
+  for(int k = start; k < amount; k++) {
+    if(m == STACKMANIP) {
+      if(tokens[k] == SPACE) { // PUSH
+	p.push_back(PUSH);
+	if(tokens[++k] == LINEFEED) { // no number as argument to PUSH
+	  throw noNumericArg;
+	} else { // we're going to parse the number now
+	  p.push_back(tokensToNumber(tokens, k)); 
+	}
+      } else if(tokens[k++] == TAB) { // COPY or SLIDE
+	if(tokens[k] == SPACE) { // COPY
+	  p.push_back(COPY);
+	} else if(tokens[k] == LINEFEED) { // COPY
+	  p.push_back(SLIDE);
+	} else throw unreachableToken;
+	// COPY and SLIDE both require a numeric argument,
+	// so we shall try to parse a number now
+	if(tokens[++k] == LINEFEED) { // no number as argument to COPY or SLIDE
+	  throw noNumericArg;
+	} else { // the actual parsing is situated in this branch
+	  p.push_back(tokensToNumber(tokens, k)); 
+	} // duplicated from PUSH
+      } else if(tokens[k++] == LINEFEED) { // DUP, SWAP or DISCARD
+	// could perhaps also be done more concisely
+	// with the ?:-operator, but i reckon something will go wrong
+	// in the exception branch
+	if(tokens[k] == SPACE) { // DUP
+	  p.push_back(DUP);
+	} else if(tokens[k] == TAB) { // SWAP
+	  p.push_back(SWAP);
+	} else if(tokens[k] == LINEFEED) { // DISCARD
+	  p.push_back(DISCARD);
+	} else throw unreachableToken;
+      } else throw unreachableToken;
+    } else if(m == ARITH) {
+    } else if(m == HEAPACC) {
+    } else if(m == FLOWCONT) {
       if(tokens[2] == LINEFEED && tokens[3] == LINEFEED) {
           exit(0);
       }
+    } else if(m == IO) {
+    } else {
+      throw unreachableToken; 
+    }
   }
-  Program p;
   return p;
 }
 
