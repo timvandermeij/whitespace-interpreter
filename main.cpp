9faf48dd7946cede3ab02f5e9303d39dd04ca12b
Tim van der Meij <timvandermeij@gmail.com>
2013-02-06 20:03:13 +0100

    Adding readFile function, fixing indentation to make it the same everywhere.

--- main.cpp
+++ main.cpp
@@ -1,66 +1,84 @@
 #include <iostream>
 #include <list>
 #include <algorithm>
+#include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
 enum Token {
-  LINEFEED,
-  SPACE,
-  TAB,
+    LINEFEED,
+    SPACE,
+    TAB,
 };
 
 enum Instruction {
-  PUSH, DUP, COPY, SWAP, DISCARD, SLIDE, // stack manipulations
-  ADD, SUB, MUL, DIV, MOD, // arithmetic operations
-  STORE, RETRIEVE, // heap access
-  MARK, CALL, JUMP, JUMPZERO, JUMPNEG, ENDSUB, ENDPROG, // flow control
-  WRITEC, WRITEN, READC, READN, // i/o operations
+    PUSH, DUP, COPY, SWAP, DISCARD, SLIDE, // stack manipulations
+    ADD, SUB, MUL, DIV, MOD, // arithmetic operations
+    STORE, RETRIEVE, // heap access
+    MARK, CALL, JUMP, JUMPZERO, JUMPNEG, ENDSUB, ENDPROG, // flow control
+    WRITEC, WRITEN, READC, READN, // i/o operations
 };
 
 class Interpreter {
-  public:
-  private:
-    list<Instruction> program;
-    list<int> heap;
-    list<int> stack; //
-    list<int> callStack; // to remember where to return to
-    unsigned int pc; // program counter
+    public:
+
+    private:
+        list<Instruction> program;
+        list<int> heap;
+        list<int> stack; //
+        list<int> callStack; // to remember where to return to
+        unsigned int pc; // program counter
 };
 
-list<Token> tokenise(const string &program) {
-  list<Token> tokens;
+list<Token> tokenize(const string &program) {
+    list<Token> tokens;
 
-  for(auto k = program.begin(); k != program.end(); ++k) {
-    switch(*k) {
-      case '\n': tokens.push_back(LINEFEED); break;
-      case ' ': tokens.push_back(SPACE); break;
-      case '\t': tokens.push_back(TAB); break;
+    for(auto k = program.begin(); k != program.end(); ++k) {
+        switch(*k) {
+            case '\n': tokens.push_back(LINEFEED); break;
+            case ' ': tokens.push_back(SPACE); break;
+            case '\t': tokens.push_back(TAB); break;
+        }
     }
-  }
-  
-  return tokens;
+    return tokens;
 }
 
 void printTokens(list<Token> tokens) {
-  int length = tokens.size();
-  for(int k = 0; k < length; ++k) {
-    switch(tokens.front()) {
-      case LINEFEED: cout << "LF"; break;
-      case SPACE: cout << "S"; break;
-      case TAB: cout << "T"; break;
+    int length = tokens.size();
+
+    for(int k = 0; k < length; ++k) {
+        switch(tokens.front()) {
+            case LINEFEED: cout << "LF"; break;
+            case SPACE: cout << "S"; break;
+            case TAB: cout << "T"; break;
+        }
+        tokens.pop_front();
+        if(k + 1 != length) {
+            cout << ":";
+        }
     }
-    tokens.pop_front();
-    if(k + 1 != length)
-      cout << ":";
-  }
+}
+
+const string readFile(const string filename) {
+    string line, fileContents;
+    ifstream input;
+
+    input.open(filename.c_str());
+    if(input.is_open()) {
+        while(!input.eof()) {
+            getline(input, line);
+            fileContents.append(line);
+        }
+    }
+    return fileContents;
 }
 
 int main() {
-    cout << "Hello whitespace" << endl;
-    //Instruction a = 500;
-    string test("\t\n \n\t   ");
-    printTokens(tokenise(test));
-    cout << endl;
+    string test = readFile("hello_world.ws");
+    cout << test << endl;
+    //string test = "\t\n \n\t   ";
+    //printTokens(tokenize(test));
     return 0;
 }
