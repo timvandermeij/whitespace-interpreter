eea2596db0c510e4c97e18a3e67dddada81eee01
willem obbens <willemobbens@gmail.com>
2013-02-06 18:57:50 +0100

    created tokenise and printTokens, updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 whitespace-interpreter
 ======================
-
 A Whitespace interpreter written in C++.
+
+instructions
+============
+Compile with ``g++ -o whitespace main.cpp -std=c++1``
+
+authors
+=======
+(in alphabetical order):
+* Simon Klaver
+* Tim van der Meij
+* Willem Obbens
+
+licence
+=======
+Not specified as of yet.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
+#include <list>
+#include <algorithm>
 
 using namespace std;
 
+enum Token {
+  LINEFEED,
+  SPACE,
+  TAB,
+};
+
+enum Instruction {
+  UNDEF,
+};
+
+class Interpreter {
+  public:
+  private:
+    list<Instruction> program;
+    list<int> heap;
+    list<int> stack; //
+    list<int> callStack; // to remember where to return to
+    unsigned int pc; // program counter
+};
+
+list<Token> tokenise(const string &program) {
+  list<Token> tokens;
+
+  for(auto k = program.begin(); k != program.end(); ++k) {
+    switch(*k) {
+      case '\n': tokens.push_back(LINEFEED); break;
+      case ' ': tokens.push_back(SPACE); break;
+      case '\t': tokens.push_back(TAB); break;
+    }
+  }
+  
+  return tokens;
+}
+
+void printTokens(list<Token> tokens) {
+  int length = tokens.size();
+  for(int k = 0; k < length; ++k) {
+    switch(tokens.front()) {
+      case LINEFEED: cout << "LF"; break;
+      case SPACE: cout << "S"; break;
+      case TAB: cout << "T"; break;
+    }
+    tokens.pop_front();
+    if(k + 1 != length)
+      cout << ":";
+  }
+}
+
 int main() {
     cout << "Hello whitespace" << endl;
+    //Instruction a = 500;
+    string test("\t\n \n\t   ");
+    printTokens(tokenise(test));
+    cout << endl;
     return 0;
 }
