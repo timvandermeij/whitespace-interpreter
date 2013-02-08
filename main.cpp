95f3ca76caaad5815ae3f6403bba834f4b98fafa
willem obbens <willemobbens@gmail.com>
2013-02-08 14:49:54 +0100

    added programToString

--- main.cpp
+++ main.cpp
@@ -502,9 +502,57 @@ Program tokensToProgram(const vector<Token> &tokens) {
     return p;
 }
 
+string programToString(Program p) {
+  int size = p.size();
+  string s;
+  for(int k = 0; k < size; k++) {
+    switch(p[k]) {
+      case PUSH: s.append("PUSH "); break;
+      case DUP: s.append("DUP"); break;
+      case COPY: s.append("COPY "); break;
+      case SWAP: s.append("SWAP"); break;
+      case DISCARD: s.append("DISCARD"); break;
+      case SLIDE: s.append("SLIDE "); break;
+      case ADD: s.append("ADD"); break;
+      case SUB: s.append("SUB"); break;
+      case MUL: s.append("MUL"); break;
+      case DIV: s.append("DIV"); break;
+      case MOD: s.append("MOD"); break;
+
+      case STORE: s.append("STORE"); break;
+      case RETRIEVE: s.append("RETRIEVE"); break;
+
+      case MARK: s.append("MARK"); break;
+      case CALL: s.append("CALL"); break;
+      case JUMP: s.append("JUMP"); break;
+      case JUMPZERO: s.append("JUMPZERO"); break;
+      case JUMPNEG: s.append("JUMPNEG"); break;
+      case ENDSUB: s.append("ENDSUB"); break;
+      case ENDPROG: s.append("ENDPROG"); break;
+
+      case WRITEC: s.append("WRITEC"); break;
+      case WRITEN: s.append("WRITEN"); break;
+      case READC: s.append("READC"); break;
+      case READN: s.append("READN"); break;
+      default: throw instructionNotFoundException;
+    }
+    switch(p[k]) {
+      case PUSH: case COPY: case SLIDE: case MARK:
+      case CALL: case JUMP: case JUMPZERO: case JUMPNEG:
+	s.append(to_string(p[++k]));
+    }
+    s.append("\n");
+  }
+  return s;
+}
+
 int main() {
     string fileContents = readFile("hello_world.ws");
-    printTokens(tokenise(fileContents));
+    auto tokens = tokenise(fileContents);
+    printTokens(tokens);
     cout << endl;
+    auto program = tokensToProgram(tokens);
+    string textrep = programToString(program);
+    cout << textrep << endl;
     return 0;
 }
